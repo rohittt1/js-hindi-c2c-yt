4ec010c15b0f86a8563349963dc6cee51461b504
rohittt1 <122153964+rohittt1@users.noreply.github.com>
2024-07-29 11:21:12 +0000

    datatypes

--- .devcontainer/01_basics/hello/01_variable.js
+++ .devcontainer/01_basics/hello/01_variable.js
@@ -2,6 +2,7 @@ const acountId = "144553"
 let accountEmail = "rohit@google.com"       
 var accountPassword = "12345"
 accountCity = "Mumbai"
+let accountState;           //o/p : undefined in js, if not given value
 
 // it is not mandatory to use semi-colon in JS..Completely optional.
 //  theoretical basic: in javascript, there are some constant keywords.
@@ -13,4 +14,4 @@ accountEmail = "hchc@hc.com"
 accountPassword = "212121"
 accountCity = "Bengaluru"  //Prefer not to use var, bcoz of issue in block scope or functional scope
 
-console.table([accountEmail, accountPassword, accountCity]);    //used for logging multiple values at one time using table method
+console.table([accountEmail, accountPassword, accountCity, accountState]);    //used for logging multiple values at one time using table method
---
+++ .devcontainer/01_basics/hello/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict";      //treats all JS code as newer version
+
+//alert statement: 
+// alert(3 + 3).. we get  this as error here, because we are using node (we have different syntax for alert here)..
+// and such operations like this are executed in web broswer console..(and get ouput as a pop-up)
+
+
+let  name = "Rohit"
+let age = 18
+let isLoggedIn = false
+let state = null;     //eg: if want temperature, server cant pass 0(coz its not applicable), hence NULL
+
+//number => range => 2 to power 53
+//bigint (used to make big web apps, eg: stock market app, facebook, reddit)
+//string = ""
+//null => standalone value
+//undefined
+// symbol (implementation mostly used in ReactJs for components)
+
+//object 
+console.log(typeof "ROhit")
+console.log(typeof undefined)   //o/p: undefined is a type in itself
+console.log(typeof null)        //o/p :whereas, null is an object (useful for interview questions)
+
